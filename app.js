eb5e6e5f88584b6b3902fa43b26e6b848a0d2c71
owenmather <owenmather7@gmail.com>
2017-08-01 23:40:16 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+var http = require("https");
+
+var options = {
+  "method": "GET",
+  "hostname": "stackoverflow.com",
+  "port": null,
+  "path": "/jobs?sort=i&l=Remote&d=20&u=Km",
+  "headers": {
+    "cache-control": "no-cache",
+  }
+};
+
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.end();
