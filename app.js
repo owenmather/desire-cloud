089f45543a9f99c6de55352e76d3b4663d9813ae
David George Mather <davidmather@live.ie>
2017-08-03 03:49:42 +0100

    s

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 	
 	function generateStackOverflowUrl(SEARCHTERM, PAGENUMBER=1){
 	  var generatedRequest = { method: 'GET',
-	  url: 'https://stackoverflow.com/jobs/remote-developer-jobs',
+	  url: 'http://localhost/stack/test.html',
 	  qs: { sort: 'i', pg: PAGENUMBER },
 	  headers: 
 	   { 'postman-token': '15718f0d-cb72-a151-6eb9-f0451ba1fa9c',
@@ -28,12 +28,44 @@
 
 	function parseStackOverflowHtml(html) {
 		const $ = cheerio.load(html);
+		const HashMap = require('hashmap');
+		var map = new HashMap();
 		$("a").each(function(i, elem) {
 			if($(this).attr("class") === "post-tag job-link no-tag-menu") {
-				console.log($(this).text());
+				if(map.get($(this).text())){
+				   map.set($(this).text(), map.get($(this).text()) + 1);
+				} else {
+				   map.set($(this).text(), 1);
+				}
 			}
 		});
 		
+		var mysql = require('mysql');
+		var pool = mysql.createPool({
+		  host     : 'localhost',
+		  user     : 'David',
+		  password : 'Qazwsx09@',
+		  database : 'desirecloud'
+		});
+
+		pool.getConnection(function(err, connection) {
+		  // Use the connection
+			
+		  	map.forEach(function(value, key) {
+				var post = {language: key, count: value};
+				console.log(key + " : " + value);
+				var query = connection.query('INSERT INTO languagelisting SET ?', post, function (error, results, fields) {
+					if (error) throw error;
+					// Neat!
+				 	
+				});
+			});
+			// And done with the connection.
+			connection.release();
+			pool.end(function (err) {
+  // all connections in the pool have ended
+});
+		});
 	};//end ParserStackOverflowHtml
 
 
